463b5fd5328ec4a783f6c8e02b05f6762f31142b
An1nhas <38216332+An1nhas@users.noreply.github.com>
2018-11-30 16:53:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="/assets/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-md fixed-top sticky" id="navbar">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand" ><img src="/assets/logos/Logo_White.png" width="110" alt="logo"></a>
+                <button class="navbar-toggle" data-toggle ="collapse" data-target =".navHeaderCollapse">
+                    <span class="glyphicon glyphicon-menu-hamburger" style="color:white; font-size:35px;"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse navHeaderCollapse">
+                <ul class="nav navbar-nav navbar-right">  
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Events</a></li>
+                    <li><a href="">Forum</a></li>
+                    <li><a href="">Contact</a></li> 
+                </ul>
+            </div>    
+        </div>
+    </nav> 
+    
+</body>
+</html>
