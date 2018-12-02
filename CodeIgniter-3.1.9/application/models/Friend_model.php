31fedad28a1307884be8828db5e0f4f4069e8a51
Husam <21155338+husam610@users.noreply.github.com>
2018-12-02 15:59:50 +0100

    Add files via upload

--- CodeIgniter-3.1.9/application/models/Friend_model.php
+++ CodeIgniter-3.1.9/application/models/Friend_model.php
@@ -12,10 +12,12 @@ class Friend_model extends CI_model
   $this->db->insert('comments', $com);
   }
 
-  public function get_all_post()
+  public function get_all_posts()
   {
     return $this->db->query("
     SELECT
+    users.user_id,
+    users.user_admin,
     users.first_name,
     posts.post_id ,
     posts.post_title,
@@ -32,14 +34,35 @@ class Friend_model extends CI_model
   public function get_all_comm($p_id)
   {
     return $this->db->query("
-    SELECT
+    SELECT 
+    users.user_id,
+    users.first_name,
+    users.user_admin,
+    posts.post_title,
+    posts.post,
+    comments.comment_id,
     comments.created_at AS comm_time ,
     comments.comment
     from comments
     join posts on comments.post_id = posts.post_id
+    join users on comments.user_id = users.user_id
     WHERE posts.post_id = $p_id
     ORDER BY comm_time DESC ;
     ")->result_array();
+  }
+  
+  public function delete_comm($id){
+      $this->db->query("
+            DELETE from comments
+            WHERE comment_id = $id ;
+      ");
+    }
+
+  public function delete_post($id){
+    $this->db->query("
+          DELETE from posts
+          WHERE post_id = $id ;
+    ");
   }
 
 
