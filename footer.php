0a5f531655d3e32a65e0849674882fc62175e201
An1nhas <38216332+An1nhas@users.noreply.github.com>
2018-12-02 11:17:40 +0100

    footer and css

---
+++ footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="/assets/style2.css">
+</head>
+<body>
+    <div class="footer-container">
+    <footer>
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <h5>CONTACT</h5>
+                <hr>
+                <a href="#">team@femaleventures.nl</a>
+            </div>
+            <div class="col-12 col-md-3">
+                <h5>FOLLOW US</h5>
+                <hr>
+                <p><a href="#">Link us on Linkedin</a></p>  
+                <p><a href="#">Like us on Facebook</a></p>
+                <p><a href="#">Follow us on Twitter</a></p>
+            </div>
+            <div class="col-12 col-md-3">
+                <h5>SUBSCRIBE TO OUR NEWSLETTER</h5>
+                <hr>
+                <form>
+                <p>Email *</p>
+                <p><input type="text" ></p>
+                <p><input type="submit" value="Subscribe!" ></p>
+                </form>
+            </div>
+            <div class="col-12 col-md-3">
+                <h5>INFORMATION</h5>
+                <hr>
+                <p><a href="#">Terms and Conditions</a></p>
+                <p><a href="#">Privacy</a></p>
+            </div>
+        </div>
+    </footer>
+    </div>
+</body>
+</html>
+
