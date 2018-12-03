89cc1b63cdebbdcc57524aa84da05573ea4f9dc2
SaltyAga <40368288+SaltyAga@users.noreply.github.com>
2018-12-03 14:47:01 +0100

    updated challenge

--- FemaleProject/application/views/friends/challenge.php
+++ FemaleProject/application/views/friends/challenge.php
@@ -52,31 +52,13 @@
      foreach ($all_post as $value)
      {echo "<hr>";
      ?>
+
      <!-- delete the post if this post for you or you are admin -->
     <form method="post" action="<?php echo base_url('friends/delete_post'); ?>">
         <input name="post_id" type="hidden" value="<?php echo $value['post_id']; ?>">
         <input type="<?php if( ($user_admin == 1 )||($value['user_id']==$user_id) ) {echo 'submit';}else{echo 'hidden';} ?>" value="Delete post"  >
     </form>
- <div>
- <?php
- if($all_post){
- foreach ($all_post as $value)
- {//echo $value['post_id']; die();
- //$this->session->set_userdata('post_id', $value['post_id']);
-//$this->session->set_userdata('post', $value['post']);
-echo "<hr>";
-echo $value['first_name']." at:".$value['pos_time']." The title is: ";
-?>
-<form style="display: inline;" method="post" action="<?php echo base_url('friends/all_comm'); ?>">
-  <input type="hidden" name="post_id" value="<?php  echo $value['post_id']; ?>" >
-  <input type="hidden" name="post" value="<?php echo $value['post']; ?>" >
-  <input type="submit" value="<?php echo $value['post_title']; ?>">
-</form>
-<?php
-} 
-}
-?>
-</div>
+
     <!-- print all the titles with author of Post -->
     <?php
     echo $value['first_name']." at:".$value['pos_time']." The title is: ";
@@ -90,6 +72,5 @@ echo $value['first_name']." at:".$value['pos_time']." The title is: ";
     } 
     }
     ?>
-</div>
 </body>
-</html> 
+</html>
