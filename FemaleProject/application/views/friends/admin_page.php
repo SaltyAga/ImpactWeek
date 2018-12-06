593232100faae1395f4ef4d6632ad458b2fe92ff
Husam <21155338+husam610@users.noreply.github.com>
2018-12-06 15:39:02 +0100

    Add files via upload

--- FemaleProject/application/views/friends/admin_page.php
+++ FemaleProject/application/views/friends/admin_page.php
@@ -1,43 +1,76 @@
-<form action="make_admin" method="post">
-	Make a new admin: 
-	<select name="nor_email">
-		<?php
-		if($all_emails_normal){
-			foreach ($all_emails_normal as $value)
-			{
-				echo "<option value='{$value['user_id']}'> {$value['email']} </option>";
-			}
-		}
-		?>
-	</select>
-	<input type="submit" value="Add">
-</form>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="assets/css/style7.css">
+</head>
+<body>
+<div class="container">
+	<div class="fb-profile">
+		<div class="main-container">
+			<div class="row">
+				<div class="col-md-6 ">
+					<div style="background-color:  #AACACD; color: white; padding: 25px; border-radius: 15px; margin-bottom: 25px;"  >
+					<form action="make_admin" method="post">
+						<div class="form-group">
+							<label >Make a new admin:</label>
 
-<form action="remove_admin" method="post">
-	Remove an admin: 
-	<select name="adm_email">
-		<?php
-		if($all_emails_admin){
-			foreach ($all_emails_admin as $value)
-			{
-				if($value['email'] !== 'admin@example.com'){
-					echo "<option value='{$value['user_id']}'> {$value['email']} </option>";
-				}
-			}
-		}
-		?>
-	</select>
-	<input type="submit" value="Remove">
-</form>
+							<select name="nor_email" class='form-control'>
+							<?php
+							if($all_emails_normal){
+							foreach ($all_emails_normal as $value)
+							{
+							echo "<option value='{$value['user_id']}'> {$value['email']} </option>";
+							}
+							}
+							?>
+							</select>
+							<input type="submit" value="Add" class="btn-info btn" style="margin: 10px;">
+						</div>
+					</form>
+					<hr>
+					<form action="remove_admin" method="post">
+						<div class="form-group">
+							<label >Remove an admin:</label>
+							<select name="adm_email" class='form-control'>
+							<?php
+							if($all_emails_admin){
+							foreach ($all_emails_admin as $value)
+							{
+							if($value['email'] !== 'admin@example.com'){
+								echo "<option value='{$value['user_id']}'> {$value['email']} </option>";
+							}
+							}
+							}
+							?>
+							</select>
+							<input type="submit" value="Remove" class="btn-danger btn" style="margin: 10px;">
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="col-md-6 ">
+				<div style="background-color:  #AACACD; color: white; padding: 25px; border-radius: 15px;"  >
+					<form action="add_event" method="post">
+						<div class="form-group">
+							<label >Add Event:</label>
+							<input type="text" name="title" placeholder="the title" class='form-control'><br>
+							<textarea name="event" placeholder="the event" class='form-control'></textarea><br>
+							<input type="text" name="location" placeholder="the location" class='form-control'><br>
+							<input type="date" name="date" class='form-control'><br>
+							<div class="input-group">
+								<span class="input-group-addon">the Price:</span>	
+								<input type="number" name="price" min="0" class='form-control'><br>
+							</div>
+							<input type="submit" value="submit" value="0" class="btn-info btn" style="margin: 10px;">
+						</div>
+					</form>
+				</div>
+			</div>					
+			</div>      
+		</div>
+	</div>	
+</div>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+</body>
 
-
-<h3>Add Event:</h3>
-<form action="add_event" method="post">
-	<input type="text" name="title" placeholder="the title"><br>
-	<textarea name="event" placeholder="the event"></textarea><br>
-	<input type="text" name="location" placeholder="the location"><br>
-	<input type="date" name="date"><br>
-	<input type="number" name="price" min="0"><br>
-	<input type="submit" value="submit" value="0">
-</form>
-<a href="all_events"> Go to events>> </a>
--- FemaleProject/application/views/friends/challenge.php
+++ FemaleProject/application/views/friends/challenge.php
@@ -1,11 +1,12 @@
-
-    <div class="post-container col-12 col-md-8 col-md-offset-4">
+<div style="margin-top: 110px; " class="container">
+    <div style="background-color:  #AACACD; color: white; padding: 25px; border-radius: 15px; margin: 15px;" class=" col-md-4 ">
+      <h3><?php echo $this->session->userdata('first_name'); ?></h3>
         <form role="form" method="post" action="add_post">
             <div class="form-group">
                 <input type="text" placeholder="The Title" name="title" class="form-control">
             </div>
             <div class="form-group">
-                <textarea type="text" placeholder="your post" name="posty" class="form-control"></textarea>
+                <textarea type="text" placeholder="your post" name="posty" class="form-control" style="height: 300px;"></textarea>
             </div>
             <input type='radio' name='public_private' value=0>
               <label>Public</label>
@@ -15,30 +16,45 @@
         </form>
     </div>
     <!-- add post + title -->
+    <div style="background-color:  #AACACD; color:  #588d8d; padding: 25px; border-radius: 15px; margin: 15px;" class="col-md-7 ">
     <?php
     $user_id=$this->session->userdata('id');
-/*@*/ $user_admin=$this->session->userdata('admin'); //we need here to know if this user admin or not. 
+    $user_admin=$this->session->userdata('admin');
      if($all_post){
      foreach ($all_post as $value)
-     {echo "<hr>";
+     {
+      echo "<hr>";
      ?>
-
+<div style="background-color: #dfebec; border-radius: 10px; padding: 10px;">
      <!-- delete the post if this post for you or you are admin -->
+     <div style="text-align: right;">
     <form method="post" action="<?php echo base_url('friends/delete_post'); ?>">
         <input name="post_id" type="hidden" value="<?php echo $value['post_id']; ?>">
-        <input type="<?php if( ($user_admin == 1 )||($value['user_id']==$user_id) ) {echo 'submit';}else{echo 'hidden';} ?>" value="Delete post"  >
+        <input class="btn btn-default" type="<?php if( ($user_admin == 1 )||($value['user_id']==$user_id) ) {echo 'submit';}else{echo 'hidden';} ?>" value="Delete post"  >
     </form>
+    </div>
 
     <!-- print all the titles with author of Post -->
-    <?php
-    echo $value['first_name']." at:".$value['pos_time']." The title is: ";
-    ?>
+    <img style="width: 15%;" class="img-circle img-thumbnail" src="/assets/images/profile/default.jpg" alt="Profile image example"/>
+    <h3 style="margin-top: 0;"><?php echo $value['first_name'];?></h3>
+    <h5><?php echo " at:".$value['pos_time']; ?></h5>
     <form style="display: inline;" method="post" action="<?php echo base_url('all_comm'); ?>">
       <input type="hidden" name="post_id" value="<?php  echo $value['post_id']; ?>" >
       <input type="hidden" name="post" value="<?php echo $value['post']; ?>" >
-      <input type="submit" value="<?php echo $value['post_title']; ?>">
+        <h4><?php echo $value['post_title']; ?></h4>
+        <p>This sidebar is as tall as its content (the links), and is always shown.
+
+Scroll this window to see the "fixed" effect.
+
+Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et.....</p>
+      <input class="btn btn-info" type="submit" value="See the post">
     </form>
+    
+  </div>
     <?php
     } 
     }
     ?>
+
+  </div>
+</div>
